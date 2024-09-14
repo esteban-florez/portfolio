df3d2e5c5c54b7328b7c03df5242a1f04144f80b
esteban-florez <eflorez077@gmail.com>
2024-09-14 18:43:23 -0400

    añadir barra de navegacion y hero de bienvenida

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,30 @@
 import './style.css'
 
-console.log('Hola mundo')
+const greeting = document.querySelector('#greeting') as HTMLElement
+const name = document.querySelector('#name') as HTMLElement
+const title = document.querySelector('#title') as HTMLElement
+const heroText = document.querySelector('#hero-text') as HTMLElement
+const heroCta = document.querySelector('#hero-cta') as HTMLElement
+
+fadeIn(greeting, 1, 0)
+fadeIn(name, 1.2, 1)
+fadeIn(title, 1.2, 1)
+fadeIn(heroText, 0.5, 2.2, false)
+fadeIn(heroCta, 0.5, 2.2, false)
+
+function fadeIn(element: HTMLElement, duration: number, delay: number, translate = true) {
+  element.style.opacity = '0'
+  if (translate) {
+    element.style.transform = 'translateY(-2rem)'
+  }
+
+  requestAnimationFrame(() => {
+    element.style.transition 
+      = `opacity ${duration}s ${delay}s, transform ${duration}s ${delay}s`
+
+    element.style.opacity = '1'
+    if (translate) {
+      element.style.transform = ''
+    }
+  })
+}
