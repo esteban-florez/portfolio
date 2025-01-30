c3f2a1dd2e6bdf7a2db3360af911f9834ada3b0c
esteban-florez <eflorez077@gmail.com>
2025-01-29 20:58:06 -0400

    adaptar carrusel a Astro, mover secciones a carpeta

---
+++ src/consts.ts
@@ -0,0 +1,20 @@
+export const REM = 16
+export const GAP = 6 * REM
+export const LOGOS = {
+  vite: 'Vite',
+  postgres: 'PostgreSQL',
+  prisma: 'Prisma',
+  nextjs: 'Next.js',
+  typescript: 'TypeScript',
+  vue: 'Vue',
+  laravel: 'Laravel',
+  mysql: 'MySQL',
+  php: 'PHP',
+  tailwind: 'TailwindCSS',
+  react: 'React',
+  github: 'GitHub',
+  javascript: 'JavaScript',
+  node: 'Node',
+  npm: 'NPM',
+  flowbite: 'Flowbite',
+} as const
--- src/scripts/carousel.ts
+++ src/scripts/carousel.ts
@@ -1,36 +1,9 @@
 import { initTooltips } from 'flowbite'
-type LogoKey = keyof typeof LOGOS
+import { REM, GAP } from '../consts'
 
-const REM = 16
-const GAP = 6 * REM
-const LOGOS = {
-  vite: 'Vite',
-  postgres: 'PostgreSQL',
-  prisma: 'Prisma',
-  nextjs: 'Next.js',
-  typescript: 'TypeScript',
-  vue: 'Vue',
-  laravel: 'Laravel',
-  mysql: 'MySQL',
-  php: 'PHP',
-  tailwind: 'TailwindCSS',
-  react: 'React',
-  github: 'GitHub',
-  javascript: 'JavaScript',
-  node: 'Node',
-  npm: 'NPM',
-  flowbite: 'Flowbite',
-} as const
-
-const tooltipContainer = document.querySelector('#tooltip-container') as HTMLElement
-const template = document.querySelector('#tooltip-template') as HTMLTemplateElement
-const carousel = document.querySelector('#carousel') as HTMLElement
 let running = true
-
-const keys = Object.keys(LOGOS) as LogoKey[]
-const imgs = keys.concat(structuredClone(keys)).map(createImage)
-
-carousel.append(...imgs)
+const carousel = document.querySelector('#carousel') as HTMLElement
+const imgs = document.querySelectorAll('[data-logo]') as NodeListOf<HTMLImageElement>
 
 carousel.addEventListener('mouseenter', () => {
   running = false
@@ -41,11 +14,16 @@ carousel.addEventListener('mouseleave', () => {
   requestAnimationFrame(animate)
 })
 
-setTimeout(() => {
-  initTooltips()
+imgs.forEach((img, index) => {
+  img.style.left = `${GAP * index + REM}px`
+  img.style.transition = 'filter 300ms'
 })
 
 requestAnimationFrame(animate)
+
+setTimeout(() => {
+  initTooltips()
+})
 
 function animate() {
   if (!running) return
@@ -63,32 +41,4 @@ function animate() {
   })
 
   requestAnimationFrame(animate)
-}
-
-function createImage(src: LogoKey, i: number) {
-  const tooltipSuffix = i < keys.length ? 'a' : 'b'
-  const img = document.createElement('img')
-  const tooltipId = `tooltip-${src}-${tooltipSuffix}`
-
-  img.src = `/logos/${src}.svg`
-  img.alt = `logo de ${src}`
-  img.setAttribute('data-tooltip-target', tooltipId)
-
-  img.classList.add('h-16', 'w-16', 'object-fill', 'absolute', 'top-[2.5rem]', 'brightness-100', 'hover:brightness-125')
-
-  img.style.left = `${GAP * i + REM}px`
-  img.style.transition = 'filter 300ms'
-
-  createTooltip(src, tooltipId)
-
-  return img
-}
-
-function createTooltip(src: LogoKey, id: string) {
-  const fragment = template.content.cloneNode(true) as DocumentFragment
-  const tooltip = fragment.children[0]
-
-  tooltip.id = id
-  tooltip.textContent = LOGOS[src]
-  tooltipContainer.append(tooltip)
 }
