46f633eabaa9acc3f924b7dfb2cc43f85d0ae33b
esteban-florez <eflorez077@gmail.com>
2025-04-29 07:58:45 -0400

    añadir barra lateral de navegacion en mobile

--- src/scripts/drawer.ts
+++ src/scripts/drawer.ts
@@ -1,11 +1,18 @@
 const drawer = document.querySelector('#drawer')
 const openBtn = document.querySelector('#drawer-open')
-const closeBtn = document.querySelector('#drawer-close')
 
 openBtn?.addEventListener('click', () => {
-  drawer?.classList.remove('-x-translate-full')
+  drawer?.classList.remove('-translate-x-full')
 })
 
-closeBtn?.addEventListener('click', () => {
-  drawer?.classList.add('-x-translate-full')
+window.addEventListener('click', (event: MouseEvent) => {
+  const target = event.target as HTMLElement
+
+  if (target.id === 'drawer-open') return
+
+  const isCloseClick = target.dataset.drawerLink
+    || !drawer?.contains(target)
+
+  if (!isCloseClick) return
+  drawer?.classList.add('-translate-x-full')
 })
